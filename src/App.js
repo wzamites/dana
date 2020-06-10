9e8a8092fdf3d07b9e4eb1da9c8dcb770481ea0a
wzamites <bj_zamites@comcast.net>
2020-06-10 01:06:03 -0400

    get the input working for one thing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import geneHash from './gene_results.js'
 
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -7,26 +8,49 @@ import FormControl from 'react-bootstrap/FormControl';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Spinner from 'react-bootstrap/Spinner'
 
 class App extends React.Component {
-  constructor() {
-      super()
+  constructor(props) {
+      super(props)
       this.state = {
-        repos: [],
+        userInput: '',
+        repos: []
       }
   }
 
-  componentDidMount() {
-    const url = "https://www.cbioportal.org/api/molecular-profiles/gbm_tcga_gistic/molecular-data?sampleListId=gbm_tcga_cna&entrezGeneId=1956&projection=SUMMARY"
-    fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then(data => {
-      this.setState({
-        repos: data
-      })
+  handleTextChange = (event) => {
+    this.setState({
+      userInput: event.target.value
     })
+  }
+
+  logThings() {
+    console.log(this.state.userInput)
+  }
+
+  callApi(gene) {
+    if (gene in geneHash) {
+      const url =
+      "https://www.cbioportal.org/api/molecular-profiles/gbm_tcga_gistic/molecular-data?sampleListId=gbm_tcga_cna&entrezGeneId="
+      + geneHash[gene]
+      + "&projection=SUMMARY"
+
+      fetch(url)
+      .then(response => response.json())
+      .then(function(data){
+        let count = 0
+        for (let patient of data) {
+          if (patient.value === 2)
+            count++
+        }
+        return count/data.length
+      })
+      .then(data => {
+        this.setState({
+          repos: data
+        })
+      })
+    }
   }
 
   render() {
@@ -38,15 +62,16 @@ class App extends React.Component {
             <InputGroup.Prepend>
               <InputGroup.Text>Enter Gene IDs Here:</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl as="textarea" aria-label="With textarea" />
+            <FormControl as="textarea" aria-label="With textarea" onChange={this.handleTextChange} />
             <InputGroup.Append>
-              <Button variant="outline-secondary">Show Chart</Button>
+              <Button variant="outline-secondary" value={this.state.userInput} onClick={() => this.callApi(this.state.userInput)}>Chart</Button>
             </InputGroup.Append>
           </InputGroup>
           </Col>
         </Row>
         <Row>
           <Col>
+          {this.state.repos}
           </Col>
         </Row>
       </Container>
